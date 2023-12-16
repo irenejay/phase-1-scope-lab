aa84deaa7dbfdcbaa05f91ff110941c98109bfff
irene <irenejim9504@gmail.com>
2023-12-16 19:20:35 +0300

    completed my scope assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = `${customerName.toUpperCase()}`;
+  }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+  }
+  
+  
+// Function to be tested
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to reassign a constant (will throw an error)
+  leastFavoriteCustomer = 'new person';
+}
+
+// Mocha test
+describe('changeLeastFavoriteCustomer()', function() {
+  it('unsuccessfully tries to reassign the least favorite customer', function() {
+    // Expecting an error to be thrown with a specific message
+    expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+  });
+});
